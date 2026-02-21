7aaaba8e57f25f5bb928946e249ac362423ee42a
Naman Singh <namansingh181807@gmail.com>
2026-02-21 10:03:35 +0530

    Day 21

---
+++ day21.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, value;
+    scanf("%d", &n);
+    
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+    
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        
+        // Allocate memory
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+        
+        if(head == NULL) {
+            head = newNode;   // First node
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    
+    // Traverse and print
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    
+    return 0;
+}
