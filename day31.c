80138e3a7bf1de793064bb2fc4f753c97d3e6e62
Naman Singh <namansingh181807@gmail.com>
2026-03-03 21:59:57 +0530

    Day 31

---
+++ day31.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = value;
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    printf("%d\n", stack[top--]);
+}
+
+void display() {
+    if (top == -1) {
+        printf("Stack is Empty\n");
+        return;
+    }
+
+    for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int op;
+        scanf("%d", &op);
+
+        if (op == 1) {
+            int value;
+            scanf("%d", &value);
+            push(value);
+        }
+        else if (op == 2) {
+            pop();
+        }
+        else if (op == 3) {
+            display();
+        }
+    }
+
+    return 0;
+}
