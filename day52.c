e7c2755d48e6f59b93c526b1a4017d2824f0032b
Naman Singh <namansingh181807@gmail.com>
2026-03-24 23:14:34 +0530

    Day 52

---
+++ day52.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create Node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Build Tree from Level Order
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct Node* root = newNode(arr[0]);
+    struct Node* queue[n];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while (i < n) {
+        struct Node* curr = queue[front++];
+
+        // Left child
+        if (arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        // Right child
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// LCA Function
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    if (root == NULL) return NULL;
+
+    if (root->data == n1 || root->data == n2)
+        return root;
+
+    struct Node* left = LCA(root->left, n1, n2);
+    struct Node* right = LCA(root->right, n1, n2);
+
+    if (left && right)
+        return root;
+
+    return (left != NULL) ? left : right;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* root = buildTree(arr, n);
+
+    struct Node* ans = LCA(root, n1, n2);
+
+    if (ans != NULL)
+        printf("%d\n", ans->data);
+
+    return 0;
+}
